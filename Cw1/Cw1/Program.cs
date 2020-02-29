ab64e822fb576c4deb6640b2b40c606ab14ab283
Karol Kosiński <s16595@pjwstk.edu.pl>
2020-02-29 17:01:25 +0100

    init project

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+        }
+    }
+}
