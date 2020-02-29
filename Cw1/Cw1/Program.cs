d72812d6c8bbb2eddbd911d5e7f74ebd42cb2d1f
Karol Kosiński <s16595@pjwstk.edu.pl>
2020-02-29 17:42:50 +0100

    done

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
+            var response = await httpClient.GetAsync("http://pjwstk.edu.pl");
+
+            Regex rx = new Regex("[A-Za-z\\d]+@[A-Za-z\\d]+\\.[A-Za-z\\d]+\\.[A-Za-z\\d]{2,}");
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var text = await response.Content.ReadAsStringAsync();
+
+                MatchCollection matches = rx.Matches(text);
+
+                foreach(Match m in matches)
+                {
+                    Console.WriteLine(m.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Code: " + response.StatusCode);
+            }
+
+           
         }
     }
 }
